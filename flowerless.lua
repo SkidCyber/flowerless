10888d7fc9c833394a666befa87412e216e439e0
titanaura <jaspermack14@zohomail.com>
2025-02-11 20:15:04 -0800

    Update flowerless.lua

--- flowerless.lua
+++ flowerless.lua
@@ -21,7 +21,7 @@ if not getgenv().SYC then
 end
 
 if not getgenv().load_game then
-    getgenv().load_game = "da_hood"
+    getgenv().load_game = "Free_Client_Script_Executor"
 end
 
 if not isfolder("flowerless") then
